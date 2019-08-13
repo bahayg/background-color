a63c0bdc2d99e5b1bf66e889777dd031fd4b42ab
Bahay Gulle Bilgi and Misaki Koonce <misaki@>
2019-08-13 11:55:13 -0700

    Add border around main text.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,34 @@
 $(document).ready(function() {
   $("button#green").click(function(){
     $("body").removeClass();
+    $("#grab-here").removeClass();
     $("body").addClass("green-background");
   });
 
   $("button#yellow").click(function() {
     $("body").removeClass();
+    $("#grab-here").removeClass();
     $("body").addClass("yellow-background");
   });
 
   $("button#red").click(function() {
     $("body").removeClass();
+    $("#grab-here").removeClass();
     $("body").addClass("red-background");
   });
   $("button#light").click(function() {
     $("body").removeClass();
+    $("#grab-here").removeClass();
     $("body").addClass("light-background");
   });
   $("button#dark").click(function() {
     $("body").removeClass();
+    $("#grab-here").removeClass();
     $("body").addClass("dark-background");
   });
+
+  $("#grab-here").click(function() {
+    $("#grab-here").removeClass();
+    $("#grab-here").addClass("highlight");
+  })
 })
